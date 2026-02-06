a2bd74bc65aa3646598457f392e53ab20e590d66
Lav Kushwaha <lavkumar062@gmail.com>
2026-02-06 16:36:33 +0530

    file updated

--- Terraform_strapi/user_data.sh
+++ Terraform_strapi/user_data.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 set -eux
 
-# Log all output for debugging
 exec > /var/log/user-data.log 2>&1
 
-# Wait for network & cloud-init
 sleep 60
 
-# Update system
 apt update -y
 
-# Install Node.js 20 (required for Strapi v5)
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-apt install -y nodejs build-essential git
+# Install dependencies
+apt install -y curl git build-essential python3
 
-# Move to ubuntu home
+# Install Node
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt install -y nodejs
+
 cd /home/ubuntu
 
-# Create Strapi app (local, not cloud)
-npx create-strapi@latest strapi-app --quickstart
+npx create-strapi@latest my-strapi --quickstart 
 
-# Build admin panel for production
-cd strapi-app
+
+cd /home/ubuntu/my-strapi
 npm run build
 
-# Start Strapi in background on boot
 nohup npm start > /home/ubuntu/strapi.log 2>&1 &
